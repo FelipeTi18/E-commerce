7268d67ade1f16b2180e824794eac870a3fd6313
Felipe Gabriel Wroblewski <gabrielfelipewroblewski@gmail.com>
2024-07-02 10:29:17 -0300

    a

--- E-commerce/mercado/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mercado.db'
-db = SQLAlchemy()
-db.init_app(app)
-
-from mercado import routes
